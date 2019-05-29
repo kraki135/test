ba18304bd83a6d7d91ad923d4fdc213cf6e88fd2
kraki135 <31313872+kraki135@users.noreply.github.com>
2019-05-29 15:01:22 +0200

    Update app.js
    wyjaśnij

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 function navMENU() {
- this.style.color = "green";   
+ this.style.color = 'red';   
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
    var trigerr = document.querySelector('.triger');
-    trigerr.addEventListener("click", navMENU());
-    
-    
+    trigerr.addEventListener("click", function() {
+    this.style.color = 'red';
+});
+});
+
+ var trigerr = document.querySelector('.triger');
+    trigerr.addEventListener("click", function() {
+    this.style.color = 'red';
 });
