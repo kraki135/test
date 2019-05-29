cd79f9b1cc740653886aaf944679e96e5a10a221
kraki135 <31313872+kraki135@users.noreply.github.com>
2019-05-29 12:02:36 +0200

    Update app.js
    triger click test

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('witaj test');
+
+var trigerr = document.getElementByClassName(.triger);	
+    trigerr.addEventListener('click', function() {
+        consol.log('haha dziala');
+        alert('test alert dziala');
+        });
+    
