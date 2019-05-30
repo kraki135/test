7dfdd5eda5870bc46dfe55c24979b23ba14fbff3
kraki135 <31313872+kraki135@users.noreply.github.com>
2019-05-30 13:20:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*
+
 
  var trigerr = document.querySelector('.triger');
  var mainNav = document.querySelector('#main');   
@@ -26,35 +26,9 @@
         this.style.color = 'red';
       }
       });
-*/
-
-//kombinowane
- var trigerr = document.querySelector('.triger');
- var mainNav = document.querySelector('#main');   
- 
- 
-     trigerr.addEventListener("click", function() {
-    
-    if (mainNav.style.display === "grid") {
-        mainNav.style.display = "none";
-        this.style.color = "green";
-      }
-    else if (mainNav.style.display === "none") {
-        mainNav.style.display = "grid";
-        this.style.color = "red"; 
-      } 
-});
 
 
-      mainNav.addEventListener('click', function() {
-        
-       if (mainNav.style.display === "grid") {
-        mainNav.style.display = "none";
+window.addEventListener('DOMContentLoaded', function() {
+mainNav.style.display = "none";
         trigerr.style.color = 'green';
-      } else if(mainNav.style.display === "none"){
-        mainNav.style.display = "grid";
-        this.style.color = 'red';
-      }
-      });
-
-
+});
