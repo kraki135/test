79c992bca23d3499172ad10dd5540f42527a15cd
kraki135 <31313872+kraki135@users.noreply.github.com>
2019-05-30 13:15:52 +0200

    Update app.js
    kombinowane

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-
-
-/**
- * function navMENU() {
- this.style.color = 'red';   
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-   var trigerr = document.querySelector('.triger');
-    trigerr.addEventListener("click", function() {
-    this.style.color = 'red';
-});
-});
- */
+/*
 
  var trigerr = document.querySelector('.triger');
  var mainNav = document.querySelector('#main');   
@@ -28,10 +15,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
 });
 
-/**nie działa */
-//var navItem = document.getElementByClassName('main');
+
       mainNav.addEventListener('click', function() {
-        //sideMenu.classList.remove('open');
+        
        if (mainNav.style.display === "grid") {
         mainNav.style.display = "none";
         trigerr.style.color = 'green';
@@ -40,33 +26,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
         this.style.color = 'red';
       }
       });
-/**
- * 
- * function navMENU() {
-    for(var i=0;max = navItem.length-1; i++) {
-        navItem[i].style.color = "black";
-        }
-        console.log('działa?');
-}
+*/
 
-
-
-var navItem = document.getElementsByClassName("nav_item");
-navItem.addEventListener("click", navMENU);
-nie działa heh
- */
+//kombinowane
+ var trigerr = document.querySelector('.triger');
+ var mainNav = document.querySelector('#main');   
+ 
+ 
+     trigerr.addEventListener("click", function() {
     
-    /**if (trigerr.style.display !== "none") { */
-        
-        
-        /**
-         * mainNav.style.display = "none";
-        trigerr.style.color = 'green';
-         */
-        
-      /**
-       * } else {
-        console.log('dupa - coś poszło nie tak');
+    if (mainNav.style.display === "grid") {
+        mainNav.style.display = "none";
+        this.style.color = "green";
       }
-       */
+    else if (mainNav.style.display === "none") {
+        mainNav.style.display = "grid";
+        this.style.color = "red"; 
+      } 
+});
+
+
+      mainNav.addEventListener('click', function() {
+        
+       if (mainNav.style.display === "grid") {
+        mainNav.style.display = "none";
+        trigerr.style.color = 'green';
+      } else if(mainNav.style.display === "none"){
+        mainNav.style.display = "grid";
+        this.style.color = 'red';
+      }
+      });
+
 
