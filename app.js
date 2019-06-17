01794fc9c0e5395148394476ed2a61bc7af69ebc
kraki135 <31313872+kraki135@users.noreply.github.com>
2019-06-17 13:22:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ trigerr.addEventListener("click", function() {
       if (trigerr.style.display === "none") {  
       
       }else if(trigerr.style.display === "block"){
-        if (mainNav.style.display === "grid") {
+        if (mainNav.style.display === 'grid' && trigerr.style.color = 'red') {
           mainNav.style.display = "none";
             trigerr.style.color = 'green';
-        } else {
+        } else if(mainNav.style.display === 'none' && trigerr.style.color = 'green'){
           mainNav.style.display = "grid";
             trigerr.style.color = 'red';
         }
