5d4b346301c1a9fa4eee255da5361e11174f8142
kraki135 <31313872+kraki135@users.noreply.github.com>
2019-06-17 13:53:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-
-
  var trigerr = document.querySelector('.triger');
  var mainNav = document.querySelector('#main');   
  
 
 
-
-
 trigerr.addEventListener("click", function() {
     if (trigerr.style.display === "none") {
-     /*consol.log("triger none - klik triger? imposible");*/
     } else {
-      /*consol.log("triger widoczny- działa?");*/
      if (mainNav.style.display === "grid") {
           mainNav.style.display = "none";
           trigerr.style.color = 'green';
@@ -22,7 +16,7 @@ trigerr.addEventListener("click", function() {
     }
    
 
-    });
+    },false);
 
 mainNav.addEventListener('click', function() {
 if (trigerr.style.display === "none") {
@@ -56,81 +50,4 @@ if (trigerr.style.display === "none") {
      
     
                                              }); ost konfiguracja
-*/
-
-/*
-mainNav.addEventListener('click', function() {
-        
-      if (trigerr.style.display === "none") {  
-      
-      }else {
-       
-        if (mainNav.style.display === "grid") {
-          mainNav.style.display = "none";
-            trigerr.style.color = 'green';
-        } else {
-          mainNav.style.display = "grid";
-            trigerr.style.color = 'red';
-        }
-      }
-     
-    
-    });
-*/
-
-    /*
-    else if (mainNav.style.display === "grid" || trigerr.style.color === "red") {
-      mainNav.style.display = "none";
-        trigerr.style.color = 'green';
-    }
-    */
-
-
-  /*
-  GŁÓWNE
-     trigerr.addEventListener("click", function() {
-    
-    if (mainNav.style.display === "none" || trigerr.style.color === "green") {
-        mainNav.style.display = "grid";
-        trigerr.style.color = 'red';
-      } else if (mainNav.style.display === "grid" || trigerr.style.color === "red") {
-        mainNav.style.display = "none";
-        trigerr.style.color = 'green';
-      }
-});
-
-
-      mainNav.addEventListener('click', function() {
-        
-       if (mainNav.style.display === "grid") {
-        mainNav.style.display = "none";
-        trigerr.style.color = 'green';
-      } else {
-        mainNav.style.display = "grid";
-        this.style.color = 'red';
-      }
-      });
-  
-  
-  
-  */
-
-
-/*
-document.addEventListener('DOMContentLoaded', function() {
-   if(trigerr.style.display !== "none") {       
-        mainNav.style.display = "none";
-        trigerr.style.color = 'green';
-   } else if(trigerr.style.display === "none"){
-        mainNav.style.display = "grid";
-        trigerr.style.display = "none";
-   }
-});
-*/
-/*
-document.addEventListener('DOMContentLoaded', function() {
-   if(trigerr.style.display === "none") {       
-        mainNav.style.display = "grid";
-   }
-});
 */
